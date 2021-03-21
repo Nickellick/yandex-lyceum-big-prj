3f993cfc941514a622ea13d1b5d633ccca7c9ceb
Nick <example@example.com>
2021-03-21 09:59:13 +0300

    Added news representation and creation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 from data import db_session
@@ -12,15 +12,31 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('KEY')
 
 
+@app.route('/create_news/<news_title>')
+def create_news(news_title):
+    db_sess = db_session.create_session()
+    news = News(title=news_title, content='Содержимое новости (тестовое)', is_private=False)
+    db_sess.add(news)
+    db_sess.commit()
+    return f'<p>Успех! Добавлена новость {news_title}</p>'
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
+
+
 def main():
     db_session.global_init('db/blogs.db')
-    db_sess = db_session.create_session()
-    user = db_sess.query(User).filter(User.id == 1).first()
-    news = News(title="Первая новость", content="Привет блог!",
-                is_private=True)
-    user.news.append(news)
-    db_sess.commit()
-    # app.run()
+    # db_sess = db_session.create_session()
+    # user = db_sess.query(User).filter(User.id == 1).first()
+    # news = News(title="Первая новость", content="Привет блог!",
+    #             is_private=True)
+    # user.news.append(news)
+    # db_sess.commit()
+    app.run(host='127.0.0.1', port=8080)
 
 
 if __name__ == '__main__':
