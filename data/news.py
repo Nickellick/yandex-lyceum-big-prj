2800ad753acca447c4fde69d1791b066b1162452
Nick <example@example.com>
2021-03-27 10:40:05 +0300

    Now we have category model

--- data/news.py
+++ data/news.py
@@ -19,3 +19,7 @@ class News(SqlAlchemyBase):
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
     user = orm.relation('User')
+
+    categories = orm.relation("Category",
+                              secondary="association",
+                              backref="news")
